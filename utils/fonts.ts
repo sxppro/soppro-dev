2e56cfdbdb6d07d488cad81260b9f9b1cddc6486
Josh <j.p.lu@outlook.com>
2023-06-10 17:25:14 +1000

    style: add custom fonts

---
+++ utils/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+const gascogne = localFont({
+  src: '../fonts/Gascogne/Gascogne.woff2',
+  weight: '400',
+  style: 'normal',
+});
+
+export { gascogne };
