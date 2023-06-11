cfcacfad77079f2a07f311161a9212d1a86f511c
Josh <j.p.lu@outlook.com>
2023-06-12 02:23:38 +1000

    style: add transparency toggle

--- components/panes/Directory.tsx
+++ components/panes/Directory.tsx
@@ -1,8 +1,10 @@
 import Name from '@/public/name.png';
 import { gascogne } from '@/utils/fonts';
-import { Box, Center, Link, Switch, VStack } from '@chakra-ui/react';
+import { Box, Center, IconButton, Link, VStack } from '@chakra-ui/react';
 import Image from 'next/image';
 import NextLink from 'next/link';
+import { useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
 const links = [
   {
@@ -24,17 +26,23 @@ const links = [
 ];
 
 const Directory = () => {
+  const [transparent, setTransparent] = useState(false);
+
   return (
     <Center
       w={'full'}
       h={'100vh'}
-      bg={'black'}
       px={16}
       py={12}
       className={gascogne.className}
+      bg={transparent ? 'transparent' : 'black'}
       flex={{ md: '0 50%' }}
       flexDir={'column'}
       justifyContent={'space-between'}
+      backdropFilter={'auto'}
+      backdropBlur={transparent ? 'sm' : 'none'}
+      boxShadow={transparent ? 'sm' : 'none'}
+      transition={'all 0.3s ease'}
     >
       <Box pos={'relative'} w={36} h={24} alignSelf={'flex-start'}>
         <Image
@@ -50,14 +58,19 @@ const Directory = () => {
             as={NextLink}
             key={link.label}
             href={link.to}
-            fontSize={'6xl'}
+            fontSize={{ base: '5xl', md: '6xl' }}
             color={'white'}
           >
             {link.label}
           </Link>
         ))}
       </VStack>
-      <Switch size="lg" />
+      <IconButton
+        icon={transparent ? <FiEyeOff /> : <FiEye />}
+        aria-label="Sidebar transparency toggle"
+        variant={'ghost'}
+        onClick={() => setTransparent(!transparent)}
+      />
     </Center>
   );
 };
